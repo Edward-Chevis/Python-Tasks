59a8da6fd1de39b2b8a8d2843a3de5efd447238e
Edward-Chevis <93992687+Edward-Chevis@users.noreply.github.com>
2021-11-10 11:29:38 +0000

    Add files via upload

---
+++ Collatz Conjecture.py
@@ -0,0 +1,12 @@
+i = 1
+number = int(input()
+while i < 10:
+             if number % 2 == 0:
+                 number = number / 2
+                 print (number)
+             elif number % 2 ==1:
+                 number = number*3
+                 number = number +1
+                 print (number)
+            else:
+             i=i+1
