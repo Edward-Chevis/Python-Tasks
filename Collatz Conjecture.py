dd43d6c31931fc1a32e2e7ad6f170ea6e6b23414
Edward-Chevis <93992687+Edward-Chevis@users.noreply.github.com>
2021-11-11 15:44:31 +0000

    Update Collatz Conjecture.py

--- Collatz Conjecture.py
+++ Collatz Conjecture.py
@@ -1,14 +1,12 @@
 # This code is designed to replicate Collatz Conjecture
 #Explaintion on Collatz Conjecture https://www.youtube.com/watch?v=094y1Z2wpJg
-i = 1
-number = int(input()
-while i < 10:
-             if number % 2 == 0:
-                 number = number / 2
-                 print (number)
-             elif number % 2 ==1:
-                 number = number*3
-                 number = number +1
-                 print (number)
-            else:
-             i=i+1
+i=1
+n=int(input('Please enter a number to prove Collatz Conjecture '))
+while i==1:
+    if n % 2 == 0:
+        n=n/2
+        print (n)
+    else:
+        n=n*3
+        n=n+1
+        print (n)
