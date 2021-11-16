df470f508584b27baf20f04ff53dcbeb49be88de
Edward-Chevis <93992687+Edward-Chevis@users.noreply.github.com>
2021-11-16 11:33:20 +0000

    Add files via upload

---
+++ Square yards calculator.py
@@ -0,0 +1,11 @@
+print ('Welcome to acme turf co')
+print ('Please enter the area you would like to cover in square yards: ')
+Sqaure_yards=float(input())
+price=Sqaure_yards * 2.7546875
+print ('Your price shall be',price)
+print ('10% off')
+price=price/10
+print(price)
+print('25% off')
+price=price/4
+print(price)
